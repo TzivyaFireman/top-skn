a129fb5e953f1eb737c45fc18816fc5d57974991
Tzivya Fireman <tz0534199716@gmail.com>
2025-12-18 12:50:55 +0200

    Refactor app.js to enable file validation

--- app.js
+++ app.js
@@ -1,71 +1,45 @@
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello World\n');
-// });
-
-// server.listen(3000, () => {
-//   console.log('Server running on port 3000');
-// });
-// const fs = require('fs');
-// const path = require('path');
-
-// function findInvalidLines(filePath) {
-//   const content = fs.readFileSync(filePath, 'utf8');
-//   const lines = content.split(/\r?\n/);
-
-//   const invalidLines = [];
-//   const invalidNumberPattern = /\d+\s+\d+/;
-
-//   for (const line of lines) {
-//     if (invalidNumberPattern.test(line)) {
-//       invalidLines.push(line);
-//     }
-//   }
-
-//   return invalidLines;
-// }
-
-// const filesDir = './files';
-// const reportPath = './report.txt';
-
-// const files = fs.readdirSync(filesDir);
-
-// let reportContent = '';
-
-// for (const file of files) {
-//   const fullPath = path.join(filesDir, file);
-
-//   if (!fs.statSync(fullPath).isFile()) continue;
-
-//   reportContent += `קובץ: ${file}\n`;
-//   reportContent += '----------------\n';
-
-//   const invalidLines = findInvalidLines(fullPath);
-
-//   if (invalidLines.length === 0) {
-//     reportContent += 'אין סעיפים לא תקינים\n';
-//   } else {
-//     invalidLines.forEach(line => {
-//       reportContent += line + '\n';
-//     });
-//   }
-
-//   reportContent += '\n';
-// }
-
-// fs.writeFileSync(reportPath, reportContent, 'utf8');
-
-
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+function normalize(num) {
+  return num.replace(/^0+/, '');
+}
+
+function isValidSectionNumber(token) {
+  return /^\d{7,}$/.test(token);
+}
+
+function extractSpacedGroups(line) {
+  const match = line.match(/(?:^|\s)((\d+\s+){2,}\d+)(?=\s|$)/);
+  return match ? match[1] : null;
+}
+
 function findInvalidLines(filePath) {
-  return fs.readFileSync(filePath, 'utf8')
-    .split(/\r?\n/)
-    .filter(line => /\d+\s+\d+/.test(line));
+  const lines = fs.readFileSync(filePath, 'utf8').split(/\r?\n/);
+
+  const knownSections = new Set();
+  const invalidLines = [];
+
+  for (const line of lines) {
+    const tokens = line.split(/\s+/);
+
+    for (const t of tokens) {
+      if (isValidSectionNumber(t)) {
+        knownSections.add(normalize(t));
+      }
+    }
+
+    const spaced = extractSpacedGroups(line);
+    if (spaced) {
+      const recomposed = normalize(spaced.replace(/\s+/g, ''));
+      if (knownSections.has(recomposed)) {
+        invalidLines.push(line);
+      }
+    }
+  }
+
+  return invalidLines;
 }
 
 const filesDir = './files';
@@ -84,3 +58,31 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => console.log('Server running on port 3000'));
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+
+// function findInvalidLines(filePath) {
+//   return fs.readFileSync(filePath, 'utf8')
+//     .split(/\r?\n/)
+//     .filter(line => /\d+\s+\d+/.test(line));
+// }
+
+// const filesDir = './files';
+
+// const server = http.createServer((req, res) => {
+//   let report = '';
+//   for (const file of fs.readdirSync(filesDir)) {
+//     const fullPath = path.join(filesDir, file);
+//     if (!fs.statSync(fullPath).isFile()) continue;
+//     report += `קובץ: ${file}\n----------------\n`;
+//     const invalidLines = findInvalidLines(fullPath);
+//     report += (invalidLines.length ? invalidLines.join('\n') : 'אין סעיפים לא תקינים') + '\n\n';
+//   }
+//   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+//   res.end(report);
+// });
+
+// server.listen(3000, () => console.log('Server running on port 3000'));
